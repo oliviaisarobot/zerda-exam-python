cb21da1267ce963083f44f6c7b563603a85c2272
Olivia Gardiner <oliv.gardiner@gmail.com>
2016-12-05 09:36:37 +0100

    Function written for second exercise.

--- second.py
+++ second.py
@@ -4,3 +4,17 @@
 # If any problem raises with the file output, the function should not break, but return False.
 # Example: when called with the following two parameters: "tree.txt", "apple",
 # the function should write "appleappleapple" to the file "tree.txt", and return True.
+
+def write_to_file_ten_times(file_name, input_string):
+    try:
+        if type(input_string) != str:
+            raise TypeError('Input is not a string')
+        else:
+            f = open(file_name, 'w')
+            f.write(input_string * 10)
+            f.close
+            return True
+    except:
+        return False
+
+print(write_to_file_ten_times("tree.txt", "apple"))
