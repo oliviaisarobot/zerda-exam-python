bcd2f929abc189423de3b9f8583aa3b676bb3a0d
Olivia Gardiner <oliv.gardiner@gmail.com>
2016-12-05 10:07:17 +0100

    Rocket class written

--- fourth.py
+++ fourth.py
@@ -22,6 +22,35 @@
 
 # The following code should work with the class:
 
+class Rocket():
+    def __init__(self, rocket_type, fuel_level=0, num_launches=0):
+        self.rocket_type = rocket_type
+        self.fuel_level = fuel_level
+        self.num_launches = num_launches
+
+    def launch(self):
+        if self.rocket_type == "falcon1":
+            if self.fuel_level >= 1:
+                self.fuel_level -= 1
+                self.num_launches += 1
+        elif self.rocket_type == "falcon9":
+            if self.fuel_level >= 9:
+                self.fuel_level -= 9
+                self.num_launches += 1
+
+    def refill(self):
+        if self.rocket_type == "falcon1":
+            output = 5 - self.fuel_level
+            self.fuel_level = 5
+            return("The rocket was filled with {} fuel.".format(output))
+        elif self.rocket_type == "falcon9":
+            output = 20 - self.fuel_level
+            self.fuel_level = 20
+            return("The rocket was filled with {} fuel.".format(output))
+
+    def getStats(self):
+        return("name: {}, fuel: {}, launches: {}".format(self.rocket_type, self.fuel_level, self.num_launches))
+
 falcon1 = Rocket('falcon1')
 returned_falcon9 = Rocket('falcon9', 11, 1)
 
